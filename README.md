7deb0f97d2e1d2bdbd8214439f8624fd0bacad01
RybakF <serhiirybalko94@gmail.com>
2022-07-14 15:34:26 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-kc17rj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-kc17rj)
---
+++ src/app/product-alert/product-alert.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
+import { Product } from '../products';
+@Component({
+  selector: 'app-product-alert',
+  templateUrl: './product-alert.component.html',
+  styleUrls: ['./product-alert.component.css']
+})
+export class ProductAlertComponent {
+
+  @Input() product!:Product;
+  @Output() notify = new EventEmitter();
+ 
+
+}
